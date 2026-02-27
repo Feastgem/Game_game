e6d50e7ac5748848b9c60cec66de7fd60da4e82e
Feastgem <dev622496@gmail.com>
2026-02-27 16:42:13 +0530

    v2

---
+++ game-project/assets/scripts/states/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+@onready var camera_pivot: Node3D = $"camera pivot"
+@export var camera_sensitvity: float = 0.005
+@export var speed :float = 20
+@export var jump: float = 10 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var bullet_spawn: Node3D = $"camera pivot/Node3D"
+var Bullet = load("res://assets/scenes/bullet.tscn")
+var flight: bool = false
+var direction : Vector3
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func _physics_process(delta: float) -> void:
+	if not is_on_floor() and not flight:
+		velocity.y -= 9.8 * delta
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	var input_dir : = Input.get_vector("left", "right", "up", "down")
+	var direction_u := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction_u
+	move_and_slide()
+	if Input.is_action_just_pressed("click"):
+		print('att')
+		var instance= Bullet.instantiate()
+		instance.position=bullet_spawn.global_position
+		instance.transform.basis=bullet_spawn.global_transform.basis
+		get_parent().add_child(instance)
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x * camera_sensitvity
+		camera_pivot.rotation.x -= event.relative.y * camera_sensitvity
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -PI/2, PI/4)
---
+++ game-project/assets/scripts/states/player/dead_player.gd
@@ -0,0 +1,10 @@
+extends state
+
+class_name dead_player
+
+func enter_state():
+	print("entering death")
+	var character = statemachine.get_parent()
+	character.velocity.x =0
+	character.velocity.z = 0
+	character.animation_player.play('dead')
